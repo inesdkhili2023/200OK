acccd4771f59489e310046d8808fe3bdf2c0a6d1
El Guebli Ons <onsgueblii@gmail.com>
2025-03-03 20:26:11 +0100

    eureuka + api

---
+++ insurance-front/src/app/user/profile/profile.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UsersService } from 'src/app/services/users.service';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent implements OnInit  {
+  constructor(private readonly userService:UsersService,
+    private readonly router: Router){}
+
+    isAdmin:boolean = false;
+    profileInfo: any;
+    errorMessage: string = ''
+
+    
+
+  async ngOnInit() {
+    this.isAdmin = this.userService.isAdmin();
+    try {
+      const token = localStorage.getItem('token')
+      if(!token){
+        throw new Error("No Token Found")
+      }
+
+      this.profileInfo = await this.userService.getYourProfile(token);
+    } catch (error:any) {
+      this.showError(error.message)
+    }
+      
+  }
+
+
+  updateProfile(id: string){
+      this.router.navigate(['/update', id])
+  }
+
+
+  showError(mess: string) {
+    this.errorMessage = mess;
+    setTimeout(() => {
+      this.errorMessage = ''
+    }, 3000)
+  }
+}
